51363b96686f73206f7eac9c74c1e33396c8a150
alexrebv <145334447+alexrebv@users.noreply.github.com>
2025-10-22 09:48:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 import os
-from bot_handlers import handle_update  # твоя логика команд и меню
+from bot_handlers import handle_update
 
 app = Flask(__name__)
 
@@ -9,7 +9,7 @@ TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
 @app.route(f"/webhook/{TELEGRAM_TOKEN}", methods=["POST"])
 def webhook():
     update = request.get_json()
-    handle_update(update)  # функция из bot_handlers.py
+    handle_update(update)
     return "OK", 200
 
 @app.route("/", methods=["GET"])
@@ -17,5 +17,4 @@ def index():
     return "Bot is running!", 200
 
 if __name__ == "__main__":
-    # Локальный запуск для теста
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
