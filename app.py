15645f35c66d524057779b8b0d7155c47ebe52f5
alexrebv <145334447+alexrebv@users.noreply.github.com>
2025-10-22 10:18:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 from flask import Flask, request
 import os
+import asyncio
 from bot_handlers import handle_update
 
 app = Flask(__name__)
 
 TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
 
+# Маршрут для webhook
 @app.route(f"/webhook/{TELEGRAM_TOKEN}", methods=["POST"])
 def webhook():
     update = request.get_json()
-    handle_update(update)
+    # Запускаем асинхронную функцию через asyncio
+    asyncio.run(handle_update(update))
     return "OK", 200
 
+# Проверка работы сервера
 @app.route("/", methods=["GET"])
 def index():
     return "Bot is running!", 200
